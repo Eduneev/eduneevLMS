ea2f78e36642344f5d5882e6db271830de4f8cb1
Sanat Sharma <sanatcolumban@gmail.com>
2019-01-29 00:01:05 -0600

    Faculty model and view

---
+++ MyLMS/Models/FacultyModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UtilityClass;
+
+namespace MyLMS.Models
+{
+    public class FacultyModel
+    {
+        public string SaveFaculty(SqlParameter[] sparams)
+        {
+            string res = "Failure..";
+            try
+            {
+                res = DAL.ExecuteScalar("SaveFaculty", sparams);
+            }
+            catch (Exception ex)
+            {
+                res = ex.Message;
+                throw;
+            }
+            return res;
+
+        }
+    }
+}
