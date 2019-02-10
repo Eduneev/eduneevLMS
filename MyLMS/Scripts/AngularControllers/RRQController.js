07750ce5943c8bcc6c03dbaf9f809073553c5818
Sanat Sharma <sanatcolumban@gmail.com>
2019-02-10 17:45:26 -0600

    Script changes for question bank and RRQ conontroller

--- MyLMS/Scripts/AngularControllers/RRQController.js
+++ MyLMS/Scripts/AngularControllers/RRQController.js
@@ -37,3 +37,70 @@ myapp.controller('NewRRQCntrl', function ($scope, $http) {
     }
 
 });
+
+myapp.controller('EditRRQCntrl', function ($scope, $http) {
+    $scope.RRQQuestionList = '';
+    SetParams(window.location.href);
+
+    $scope.currentPage = 0;
+    $scope.RRQpageSize = 5;
+    $scope.RRQnumberOfPages = function () {
+        return Math.ceil($scope.RRQQuestionList.length / $scope.RRQpageSize);
+    }
+
+    function SetParams(url) {
+        try {
+            var params = parseURLParams(url);
+            if ('RRQ_ID' in params) {
+                $scope.RRQID = params['RRQ_ID'][0];
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    $scope.AddQuestion = function () {
+        window.location.href = "/SessionMgmt/AddQuestionsToRRQ/" + $scope.RRQID;
+    }
+
+    // Get RRQ Questions
+    $scope.GetRRQQuestionDetails = function () {
+        $http.get('/QuestionBank/GetRRQQuestions')
+            .then(function (result) {
+                $scope.RRQQuestionList = result.data;
+                console.log($scope.RRQQuestionList)
+            });
+    }
+
+    // Remove Question From RRQ
+    $scope.RemoveQuestionFromRRQ = function (QID) {
+        $http({
+            method: 'POST',
+            url: '/QuestionBank/AddRemoveQuestionToRRQ',
+            data: { QID: QID }
+        }).then(function (result) {
+            $scope.GetRRQQuestionDetails();
+        });
+    }
+
+    function parseURLParams(url) {
+        var queryStart = url.indexOf("?") + 1,
+            queryEnd = url.indexOf("#") + 1 || url.length + 1,
+            query = url.slice(queryStart, queryEnd - 1),
+            pairs = query.replace(/\+/g, " ").split("&"),
+            parms = {}, i, n, v, nv;
+
+        if (query === url || query === "") return;
+
+        for (i = 0; i < pairs.length; i++) {
+            nv = pairs[i].split("=", 2);
+            n = decodeURIComponent(nv[0]);
+            v = decodeURIComponent(nv[1]);
+
+            if (!parms.hasOwnProperty(n)) parms[n] = [];
+            parms[n].push(nv.length === 2 ? v : null);
+        }
+        return parms;
+    }    
+});
