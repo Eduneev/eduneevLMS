49ad7651918149167e45d1cf00c38b5a66198671
Sanat Sharma <sanatcolumban@gmail.com>
2019-04-30 01:26:34 -0500

    js Student controller

--- MyLMS/Scripts/AngularControllers/StudentController.js
+++ MyLMS/Scripts/AngularControllers/StudentController.js
@@ -155,6 +155,10 @@ myapp.controller('StudentViewCntrl', function ($scope, $http) {
         $scope.StudentID = StudentID;
     }
 
+    $scope.EditStudent = function (code) {
+        window.location.href = "/StudentMgmt/EditStudent?StudentCode=" + code;
+    }
+
     $scope.SaveImage = function () {
         debugger;
         //var _StudentImage = $('#mce').contents().find('#select_name').val();
@@ -180,6 +184,174 @@ myapp.controller('StudentViewCntrl', function ($scope, $http) {
         delete $scope.result;
         delete $scope.resultBlob;
     };
+});
+
+myapp.controller('EditStudentCntrl', function ($scope, $http) {
+
+    SetParams(window.location.href);
+
+    $scope.StudentSubjects = {};
+    $scope.SubjectSet = new Set();
+
+    function SetParams(url) {
+        try {
+            var params = parseURLParams(url);
+            if ('StudentCode' in params) {
+                $scope.StudentCode = params['StudentCode'][0];
+                GetStudentDetails();
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    // Create a SubjectSet that tracks the subjects the student is registered for
+    function GetStudentDetails() {
+        $http.get('/StudentMgmt/GetStudentsByCode/' + $scope.StudentCode)
+            .then(function (result) {
+                var data = result.data;
+
+                console.log(data)
+                for (let i = 0; i < data.length; i++) {
+                    $scope.StudentSubjects[data[i].SubjectID] = data[i].StudentID;
+                    $scope.SubjectSet.add(data[i].SubjectID);
+                }
+
+                if (data.length > 0) {
+                    $scope.StudentName = data[0].StudentName;
+                    $scope.Code = data[0].Code;
+                    $scope.Gender = data[0].Gender;
+                    $('#drpGender').val($scope.Gender);
+
+                    $scope.ProgID = data[0].ProgID;
+                    $scope.ProgramsList = [{ 'ProgID': $scope.ProgID, 'ProgramName': data[0].ProgramName }];
+                    console.log($scope.ProgramsList)
+                    $scope.CourseID = data[0].CourseID;
+                    $scope.CoursesList = [{ 'CourseID': $scope.CourseID, 'CourseName': data[0].CourseName }];
+
+                    $scope.Email = data[0].Email;
+                    $scope.Mobile = data[0].Mobile;
+                    $scope.Landline = data[0].Landline;
+                    $scope.GuardianName = data[0].GuardianName;
+                    $scope.GuardianContactNo = data[0].GuardianContactNo;
+                    $scope.SchoolName = data[0].SchoolName;
+                    $scope.Address = data[0].Address;
+                    $scope.PinCode = data[0].PinCode;
+                    $scope.StudentImageURL = data[0].StudentImageURL;
+
+                    GetSubjectsList();
+                }
+            });
+    }
+
+    $scope.AddRemoveSubject = function (SubjectID) {
+        if ($scope.SubjectSet.has(SubjectID))
+            $scope.SubjectSet.delete(SubjectID);
+        else
+            $scope.SubjectSet.add(SubjectID);
+
+        console.log($scope.SubjectSet);
+    };
+
+    function GetSubjectsList() {
+        $http.get('/CourseMgmt/GetSubject/' + $scope.CourseID)
+            .then(function (result) {
+                $scope.SubjectsList = result.data;
+
+                console.log($scope.StudentSubjects)
+                for (var i = 0; i < $scope.SubjectsList.length; i++) {
+                    if ($scope.SubjectsList[i].SubjectID in $scope.StudentSubjects)
+                        $scope.SubjectsList[i].selected = true;
+                    else
+                        $scope.SubjectsList[i].selected = false;
+                }
+                console.log($scope.SubjectsList)
+            });
+    };
+
+    // When one of the subjects is removed from the set, call delete StudentFunctionality 
+    // with that SubjectID and StudentID, Else for any new subjects added, call SaveStudent
+    // For the remaining subjects that have been neither added nor removed, loop through
+    // and call EditStudent with StudentID
+    $scope.EditStudent = function () {
+        for (SubjectID of $scope.SubjectSet) {
+
+            var _StudentName = $scope.StudentName;
+            var _Code = $scope.Code;
+            var _Gender = $scope.Gender;
+            var _ProgramID = $scope.ProgID;
+            var _CourseID = $scope.CourseID;
+            var _SubjectID = SubjectID;
+            var _Email = $scope.Email;
+            var _Mobile = $scope.Mobile;
+            var _Landline = $scope.Landline;
+            var _GuardianName = $scope.GuardianName;
+            var _GuardianContactNo = $scope.GuardianContactNo;
+            var _BirthPlace = $scope.BirthPlace;
+            var _SchoolName = $scope.SchoolName;
+            var _Address = $scope.Address;
+            var _Pincode = $scope.PinCode;
+
+            //Edit Student
+            if (SubjectID in $scope.StudentSubjects) {
+                $http({
+                    method: 'POST',
+                    url: '/StudentMgmt/EditStudent',
+                    data: { StudentID: $scope.StudentSubjects[SubjectID], StudentName: _StudentName, Code: _Code, Gender: _Gender, ProgramID: _ProgramID, CourseID: _CourseID, SubjectID: _SubjectID, Email: _Email, Mobile: _Mobile, Landline: _Landline, GuardianName: _GuardianName, GuardianContactNo: _GuardianContactNo, BirthPlace: _BirthPlace, SchoolName: _SchoolName, Address: _Address, Pincode: _Pincode }
+                }).then(function (result) {
+                });
+            }
+            // Save Student
+            else {
+                $http({
+                    method: 'POST',
+                    url: '/StudentMgmt/SaveStudent',
+                    data: { StudentName: _StudentName, Code: _Code, Gender: _Gender, ProgramID: _ProgramID, CourseID: _CourseID, SubjectID: _SubjectID, Email: _Email, Mobile: _Mobile, Landline: _Landline, GuardianName: _GuardianName, GuardianContactNo: _GuardianContactNo, BirthPlace: _BirthPlace, SchoolName: _SchoolName, Address: _Address, Pincode: _Pincode }
+                }).then(function (result) {
+                });
+            }
+        }
+
+        for (SubjectID in $scope.StudentSubjects) {
+            if (!(SubjectID in $scope.SubjectSet)) {
+                // Delete Student
+                $http({
+                    method: 'POST',
+                    url: '/StudentMgmt/DeleteStudent',
+                    data: { StudentID: $scope.SubjectSet[SubjectID] }
+                }).then(function (result) {
+                });
+
+            }
+
+        }
+        alert("Student Saved Successfully");
+    }
+
+    $scope.ViewStudentsList = function () {
+        window.location.href = "/StudentMgmt/ViewStudents/";
+    }
+
+    function parseURLParams(url) {
+        var queryStart = url.indexOf("?") + 1,
+            queryEnd = url.indexOf("#") + 1 || url.length + 1,
+            query = url.slice(queryStart, queryEnd - 1),
+            pairs = query.replace(/\+/g, " ").split("&"),
+            parms = {}, i, n, v, nv;
+
+        if (query === url || query === "") return;
+
+        for (i = 0; i < pairs.length; i++) {
+            nv = pairs[i].split("=", 2);
+            n = decodeURIComponent(nv[0]);
+            v = decodeURIComponent(nv[1]);
+
+            if (!parms.hasOwnProperty(n)) parms[n] = [];
+            parms[n].push(nv.length === 2 ? v : null);
+        }
+        return parms;
+    }
 });
 
 myapp.controller('StudentAttendanceCntrl', function ($scope, $http) {
