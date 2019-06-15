266c60aaf1c965f8619804d8e12869c29d9c34ff
Sanat Sharma <sanatcolumban@gmail.com>
2019-06-14 23:19:04 -0700

    Add stuff to classroom model

--- MyLMS/Models/ClassRoomModel.cs
+++ MyLMS/Models/ClassRoomModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
+using UtilityClass;
 
 namespace MyLMS.Models
 {
@@ -12,6 +14,36 @@ namespace MyLMS.Models
         public int CenterId { get; set; }
         public string CenterName { get; set; }
         public string LastUsedCommand { get; set; }
+
+        public string SaveClassRoom(SqlParameter[] sparams)
+        {
+            string res = "Failure..";
+            try
+            {
+                res = DAL.ExecuteScalar("SaveClassroom", sparams);
+            }
+            catch (Exception ex)
+            {
+                res = ex.Message;
+                throw;
+            }
+            return res;
+        }
+
+        public string DeleteClassRoom(SqlParameter[] sparams)
+        {
+            string res = "Failure..";
+            try
+            {
+                res = DAL.ExecuteScalar("DeleteClassroom", sparams);
+            }
+            catch (Exception ex)
+            {
+                res = ex.Message;
+                throw;
+            }
+            return res;
+        }
     }
 
 }
