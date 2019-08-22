d7b18c0553949b7ce16f03390d2fc6f0ea923382
Sanat Sharma <sanatcolumban@gmail.com>
2019-08-22 12:03:57 +0530

    RRQ Report model

---
+++ MyLMS/Models/RRQReport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyLMS.Models
+{
+    public class RRQDetails
+    {
+        public int RRQID { get; set; }
+        public string RRQNo { get; set; }
+        public List<Dictionary<Int32,Option>> Questions { get; set; }
+        public int TotalMarks { get; set; }
+    }
+
+    public class StudentReport
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public List<Dictionary<Int32,Option>> Responses { get; set; }
+        public int TotalMarks { get; set; }
+    }
+
+}
