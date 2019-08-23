0a4a58e80303b2066b672cf9107c2edf97086b6e
Sanat Sharma <sanatcolumban@gmail.com>
2019-08-23 12:56:07 +0530

    Create RRQ Report controller

---
+++ MyLMS/Controllers/RRQReportController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyLMS.Models;
+
+namespace MyLMS.Controllers
+{
+    public class RRQReportController : Controller
+    {
+        public ActionResult RRQReport()
+        {
+            return View();
+        }
+
+        public ActionResult RRQDashboard(int id)
+        {
+            DashboardData DashboardObj = new DashboardData();
+            DashboardObj.GetRespPrcnt(id);
+            ViewBag.VBDashboard = DashboardObj;
+            return View();
+        }
+
+    }
+}
