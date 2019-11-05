9497e77b0cf7dca410e9a8fe894ddca3ee7da909
Sanat Sharma <sanatcolumban@gmail.com>
2019-11-04 22:17:45 -0600

    auth processing

---
+++ MyLMS/Scripts/auth_processing.bat
@@ -0,0 +1,21 @@
+set arg1=%1
+echo %arg1%
+touch auth.pem
+echo "Creating auth file"
+echo %arg1% > auth.pem
+if exist "./2WayLive.zip" (
+    echo "Removing old zip"
+    rm "./2WayLive.zip"
+)
+
+
+echo "downloading 2WayLive package..."
+wget https://sanats.s3-us-west-2.amazonaws.com/2WayLive.zip
+
+unzip 2WayLive.zip
+cp ".\auth.pem" ".\2WayLive\Center Login"
+mv ".\auth.pem" ".\2WayLive\2WayCall"
+echo "zipping again"
+zip -r 2WayLive.zip "./2WayLive"
+rm -rf 2WayLive
+
