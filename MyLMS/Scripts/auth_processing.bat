70802f895c4efc2ca7bf611ac7449a08d1d04da8
Sanat Sharma <sanatcolumban@gmail.com>
2019-11-06 17:06:08 -0600

    auth processing final changes...hopefully

--- MyLMS/Scripts/auth_processing.bat
+++ MyLMS/Scripts/auth_processing.bat
@@ -1,3 +1,5 @@
+cd %2
+echo %cd%
 set arg1=%1
 echo %arg1%
 echo "Creating auth file"
@@ -9,11 +11,10 @@ if exist "./2WayLive.zip" (
 
 echo "downloading 2WayLive package..."
 wget https://sanats.s3-us-west-2.amazonaws.com/2WayLive.zip
-
 unzip 2WayLive.zip
 copy ".\auth.pem" ".\2WayLive\Center Login"
 move ".\auth.pem" ".\2WayLive\2WayCall"
 echo "zipping again"
 zip -r 2WayLive.zip "./2WayLive"
 rmdir /Q /S 2WayLive
-
+exit
