557420a676e9abd845e9a07062920712d7335851
Sanat Sharma <sanatcolumban@gmail.com>
2019-12-18 16:35:43 +0530

    new changes

--- MyLMS/Scripts/auth_processing.bat
+++ MyLMS/Scripts/auth_processing.bat
@@ -5,17 +5,23 @@ echo %arg2%
 cd %arg2%
 echo "Creating auth file"
 echo %arg1% > auth.pem
-if exist "./2WayLive.zip" (
+if not exist "./2WayLive" (
+    
     echo "Removing old zip"
     del "./2WayLive.zip"
+    echo "downloading 2WayLive package..."
+    wget https://sanats.s3-us-west-2.amazonaws.com/2WayLive.zip
+    unzip 2WayLive.zip
 )
 
-echo "downloading 2WayLive package..."
-wget https://sanats.s3-us-west-2.amazonaws.com/2WayLive.zip
-unzip 2WayLive.zip
+rem Need to have method to check if S3 2WayLive zip has changed
+
 copy ".\auth.pem" ".\2WayLive\Center Login"
 move ".\auth.pem" ".\2WayLive\2WayCall"
 echo "zipping again"
 zip -r 2WayLive.zip "./2WayLive"
-rmdir /Q /S 2WayLive
+
+rem remove auth files so we can reuse it
+del ".\2WayLive\Center Login\auth.pem"
+del ".\2WayLive\2WayCall\auth.pem"
 exit
