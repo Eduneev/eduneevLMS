3177f77ffa3f1f74d0a2614c8f2ae089c263f3da
Sanat Sharma <sanatcolumban@gmail.com>
2020-12-07 23:38:29 -0600

    Center controller update

--- MyLMS/Scripts/AngularControllers/CenterController.js
+++ MyLMS/Scripts/AngularControllers/CenterController.js
@@ -353,7 +353,7 @@ myapp.controller('OrgCntrl', function ($scope, $http) {
         var result = "SET /a classroom=" + ClassroomID + "\n";
         result = result + "SET /a i=0\nsetlocal EnableDelayedExpansion\nSET RES=\nFOR /F %%X IN ('wmic path win32_computersystemproduct get uuid') DO (";
         result = result + "SET VAR=%%X\nIF !i! EQU 1 (SET RES=!VAR!)\nSET /a i+=1\n@echo !i!\n)\n";
-        result = result + "SET URL=http://portal.2WayLive.com/api/SetClassroomAuth/!classroom!/!RES!\ncurl !URL!";
+        result = result + "SET URL=https://portal.2WayLive.com/api/SetClassroomAuth/!classroom!/!RES!\ncurl !URL!";
         name = $scope.CenterName + "-" + ClassroomName + ".bat";
         var a = $('<a/>', {
             style: 'display:none',
@@ -384,7 +384,7 @@ myapp.controller('OrgCntrl', function ($scope, $http) {
                 
                 var a = $('<a/>', {
                     style: 'display:none',
-                    href: "http://portal.2WayLive.com/Scripts/2WayLive.zip",
+                    href: "https://portal.2WayLive.com/Scripts/2WayLive.zip",
                     download: '2WayLive.zip'
                 }).appendTo('body');
                 a[0].click();
@@ -429,4 +429,48 @@ myapp.controller('AllocateReceiverCntrl', function ($scope, $http) {
             GetClassroomList();
         });
     };
+});
+
+
+myapp.controller('CenterPollCntrl', function ($scope, $http) {
+
+    GetSessionsForCenter();
+    $scope.SessionID = 0;
+    $scope.CenterID = 0;
+
+    function GetSessionsForCenter() {
+        $http.get('/SessionMgmt/GetSessionsForCenter')
+            .then(function (result) {
+                $scope.SessionsList = result.data;
+            });
+    }
+
+    function GetStudentsByCenterID() {
+        $http.get('/SessionMgmt/GetStudentsAttendanceByCenterID/' + $scope.CenterID + '/' + $scope.SessionID)
+            .then(function (result) {
+                $scope.StudentsList = result.data;
+            });
+    }
+
+    $scope.SetValues = function (SessionID, CenterID) {
+        $scope.SessionID = SessionID;
+        $scope.CenterID = CenterID;
+
+        GetStudentsByCenterID();
+    };
+
+    $scope.SavePollResponse = function (OptionSeq) {
+        var _SessionID = $scope.SessionID;
+        var _CenterID = $scope.CenterID;
+        var _OptionSeq = OptionSeq;
+
+        $http({
+            method: 'POST',
+            url: '/SessionMgmt/SavePollResponse',
+            data: { SessionID: _SessionID, CenterID: _CenterID, OptionSeq: _OptionSeq }
+        }).then(function (result) {
+            alert("Poll response posted");
+        });
+    };
+
 });
